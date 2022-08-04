be1f64f7ebe62f23d50753e852decfd64b8ec2b6
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-08-04 17:17:51 +0100

    Female class

---
+++ female.py
@@ -0,0 +1,22 @@
+from human import Human
+
+class Female(Human):
+
+    def __init__(self):
+        super().__init__()
+        self.fav_colour = "white"
+        self.breathe = True
+        self.hair = True
+
+    def dance(self):
+        return "You can learn to dance whether you have been born with natural talent or not."
+
+    def code(self):
+        return "Ada Lovelace is credited for being the world's first programmer"
+
+    def drive(self):
+        return "Enjoy driving"
+
+female_object = Female()
+print(female_object.hair)
+print(female_object.code())
